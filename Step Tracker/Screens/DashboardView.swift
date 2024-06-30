7fa48a1b1bc26bff65c0a3e859b43ac9dc1c790f
Jason Fang <jasonfang623@gmail.com>
2024-06-30 16:06:02 -0400

    50% done

--- Step Tracker/Screens/DashboardView.swift
+++ Step Tracker/Screens/DashboardView.swift
@@ -47,6 +47,7 @@ struct DashboardView: View {
                         StepPieChart(chartData: ChartMath.averageWeekdayCount(for: hkManager.stepData))
                     case .weight:
                         WeightLineChart(selectedStat: selectedStat, chartData: hkManager.weightData)
+                        WeightDiffBarChart(chartData: ChartMath.averageDailyWeightDiffs(for: hkManager.weightDiffData))
                     }
                    
                 }
@@ -55,7 +56,7 @@ struct DashboardView: View {
             .task {
                 await hkManager.fetchStepCount()
                 await hkManager.fetchWeights()
-                ChartMath.averageDailyWeightDiffs(for: hkManager.weightDiffData)
+                await hkManager.fetchWeightsForDifferentials()
                 isShowingPermissionPrimingSheet = !hasSeenPermissionPriming
             }
             .navigationTitle("Dashboard")
